4246a27748c5c12cb132fb4dba9fdbd7f57e5046
Mahesh Rajendra Bahir <107680212+MaheshB45@users.noreply.github.com>
2024-01-13 12:19:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function nextPalindrome(num) {
-  //your JS code here.
+function isPalindrome(str) {
+  // Check if the given string is a palindrome
+  return str === str.split('').reverse().join('');
 }
 
-const input = prompt("Enter a palindrome number");
+function nextPalindrome(num) {
+  // Increment the number and check if it is a palindrome
+  while (true) {
+    num++;
+    if (isPalindrome(num.toString())) {
+      return num;
+    }
+  }
+}
+
+// Example usage
+const input = parseInt(prompt("Enter a palindrome number"));
 alert(nextPalindrome(input));
